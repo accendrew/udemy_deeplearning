5718dae8ac631ee62a28c61a0327012880a74e28
Andrew Howard <andrew.r.howard@accenture.com>
2020-01-07 14:56:40 -0500

    Updated code in homework solution to execute properly.

--- ANN/ann_homework_solution.py
+++ ANN/ann_homework_solution.py
@@ -22,49 +22,53 @@ X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
 # Encoding categorical data
+# Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X_1 = LabelEncoder()
-X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+from sklearn.compose import ColumnTransformer
+
+# Encode Male/Female to number
 labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-onehotencoder = OneHotEncoder(categorical_features = [1])
-X = onehotencoder.fit_transform(X).toarray()
+
+# Convert three country values (Spain, France, Germany) into boolean vales.
+ct = ColumnTransformer([("Country", OneHotEncoder(), [1])], remainder="passthrough")
+X = ct.fit_transform(X)
+# Remove dummy variable, only need 2 to uniquely determine country
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
+
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Part 2 - Now let's make the ANN!
-
-# Importing the Keras libraries and packages
+# Import Keras package and libraries
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-# Initialising the ANN
 classifier = Sequential()
 
-# Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+# First hidden layer, with 11 inputs
+classifier.add(Dense(6, input_dim=11, kernel_initializer='uniform', activation='relu'))
 
-# Adding the second hidden layer
-classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+# Second hidden layer
+classifier.add(Dense(6, kernel_initializer='uniform', activation='relu'))
 
-# Adding the output layer
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+# Output layer
+classifier.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
 
-# Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# Compile ANN
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Fitting the ANN to the Training set
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+# Fit ANN to training set
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
 
 # Part 3 - Making predictions and evaluating the model
 
