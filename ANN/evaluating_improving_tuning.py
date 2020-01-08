4e687c59e352fd8e3349dc67173f678d1704b85c
Andrew Howard <andrew.r.howard@accenture.com>
2020-01-08 11:19:58 -0500

    Grid tuning algorithm implemented.

--- ANN/evaluating_improving_tuning.py
+++ ANN/evaluating_improving_tuning.py
@@ -49,21 +49,54 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Import Keras package and libraries
-import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 
+#
+# # Evaluate ANN
+# def build_classifier():
+#     classifier = Sequential()
+#     classifier.add(Dense(6, input_dim=11, kernel_initializer='uniform', activation='relu'))
+#     classifier.add(Dense(6, kernel_initializer='uniform', activation='relu'))
+#     classifier.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
+#     classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+#     return classifier
+#
+#
+# # K-Fold Cross Validation repeats training and evaluation against revolving subsets of training and test data.
+# classifier = KerasClassifier(build_fn=build_classifier, batch_size=10, epochs=10)
+# accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, n_jobs=-1)
+# mean = accuracies.mean()
+# variance = accuracies.std()
 
-def build_classifier():
+# Tuning the ANN
+# Apply grid search technique to test the effects of different combinations of model parameters on model accuracy
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+
+
+# Optimizer function now parameterized
+def build_classifier(optimizer):
     classifier = Sequential()
     classifier.add(Dense(6, input_dim=11, kernel_initializer='uniform', activation='relu'))
     classifier.add(Dense(6, kernel_initializer='uniform', activation='relu'))
     classifier.add(Dense(1, kernel_initializer='uniform', activation='sigmoid'))
-    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
     return classifier
 
 
-classifier = KerasClassifier(build_fn=build_classifier, batch_size=10, nb_epoch=10)
-accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, n_jobs=-1)
+# Attempt model training with different values for batch_size, nb_epoch, and optimizer.
+classifier = KerasClassifier(build_fn=build_classifier)
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 500],
+              'optimizer': ['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator=classifier, param_grid=parameters, scoring='accuracy', cv=10)
+grid_search = grid_search.fit(X_train, y_train)
+
+# Best results, with associated model parameters
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
